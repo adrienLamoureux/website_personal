e2c437aeef2adc22d4c2fb382fbdef3d5df587d3
adrienLamoureux <vergil1534@gmail.com>
2015-03-28 18:37:44 +0100

    accueil

--- client/js/controllers/directives/navigation.js
+++ client/js/controllers/directives/navigation.js
@@ -1,17 +1,9 @@
 // NavigationBar Controller
 app.controller('NavigationCtrl', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout){
-		$scope.activeMenu=0;
-		$scope.activeSubMenu=0;
-
+	$scope.activeMenu=0;
+	$scope.userConnected=false; //TODO : gestion de la connexion
 	// change Menu state
 	$scope.changeActiveMenu = function(id) {
-		$scope.activeMenu=id;
-		if(id != 5)
-			$scope.activeSubMenu=0;
-	};
-
-	// change SubMenu state
-	$scope.changeActiveSubMenu = function(id) {
-		$scope.activeSubMenu=id;
+	$scope.activeMenu=id;
 	};
 }]);
